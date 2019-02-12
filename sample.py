167313a9c1dad54a3b42513c63cfeaa1ebe9cf86
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-02-12 15:58:20 -0800

    fixing

--- sample.py
+++ sample.py
@@ -4,30 +4,42 @@ from histogram import histogram
 with open('sample.words', 'r') as f:
     lines = f.read().split(" ")
 
-def rand_word(hist):
-    count = 0 
-    for word, freq in hist.items():
-        print(word)
-        print(freq)
-    # lis = []
-    # for word in hist:
-    #     lis.append(word)
-    
-    # return random.choice(lis)
+def freq(hist):
+    ''' take a random word from a histogram, based on freq'''
+    output = random.sample(list(hist), 1)
+
+    return ''.join(output)
+
+    #print(random.choice(hist.keys()))
+
+    # return random.choice(hist.keys())
+    # output = []
+    # for word, freq in hist.items():
+    #     return random.choice(hist.keys())
+    # print(hist)
+    # return random.choice(hist)
+    # count = 0 
+    # output = []
+
+    # for word, freq in hist.items():
+    #     count += freq
+    #     print("{} appears {} time(s). The count is {}".format(word, freq, count))
+    #     # if freq < count:
+    #     #     print(word)
 
 def rand_test(hist):
-    ''' run the random word function a bunch of times '''
-    word = rand_word(hist)
+    ''' run the random word function a x times '''
+    word = freq(hist)
+    x = 10000
     output = []
 
     i = 0
-    while i <= 10000:
-        word = rand_word(hist)
+    while i <= x:
+        word = freq(hist)
         output.append(word)
         i += 1
 
-    print(histogram(output))
-    #print(output)
+    return histogram(output)
 
     
 
@@ -50,20 +62,12 @@ def rand_test(hist):
 #         if cum_prob >= rand_num:
 #             return val[0]
 
-# def multiple_runs(lines):
-#     count = {}
-
-#     for item in lines:
-#         count[item[0]] = 0
-
-#     for i in range(0, 1000):
-#         count[sample(lines)] += 1
     
 if __name__ == '__main__':
-    my_histogram = histogram(lines)
-    random_word = rand_word(my_histogram)
-    print('Random Word: {}'.format(random_word))
-    random_test = rand_test(my_histogram)
-    print('Testing Randomness: {}'.format(random_test))
-    # multiple_runs(lines)
+    hist = histogram(lines)
+    rand = freq(hist)
+    test = rand_test(hist)
+    
+    print(test)
+
     

15c72741a79ab391473efd92281a39231f94d36e
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-02-12 18:46:15 -0800

    fixing

--- dictionary_words.py
+++ dictionary_words.py
@@ -3,12 +3,13 @@ from random import sample
 
 # with automatically closes the file once everything inside of it has been done
 with open('sample.words', 'r') as f:
-    words_list = [line.strip() for line in f]
+    lines = f.read().split(" ")
+    #words_list = [line.strip('') for line in f]
 
 
 def sentence(): 
-    output = ' '.join(random.sample(words_list, 9)) + '.'
+    output = ' '.join(random.sample(lines, 9)) + '.'
     return output.capitalize()
 
 if __name__ == '__main__':
-    sentence()
+    print(sentence())

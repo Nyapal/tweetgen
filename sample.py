51628f9c243990e939df85b87c50f037a10a99e3
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-02-12 16:30:08 -0800

    stochasting sampling working

--- sample.py
+++ sample.py
@@ -4,31 +4,27 @@ from histogram import histogram
 with open('sample.words', 'r') as f:
     lines = f.read().split(" ")
 
+def single_rand_word(hist):
+    ''' returns a random word from a histogram '''
+    return ''.join(random.sample(list(hist), 1))
+
 def freq(hist):
-    ''' take a random word from a histogram, based on freq'''
-    output = random.sample(list(hist), 1)
+    ''' returns a random word from a histogram, based on freq'''
+    count = 0 
+    dic = histogram(hist)
+    for key in dic:
+        count += dic[key]
 
-    return ''.join(output)
-
-    #print(random.choice(hist.keys()))
-
-    # return random.choice(hist.keys())
-    # output = []
-    # for word, freq in hist.items():
-    #     return random.choice(hist.keys())
-    # print(hist)
-    # return random.choice(hist)
-    # count = 0 
-    # output = []
-
-    # for word, freq in hist.items():
-    #     count += freq
-    #     print("{} appears {} time(s). The count is {}".format(word, freq, count))
-    #     # if freq < count:
-    #     #     print(word)
+    rand = random.randrange(count)
+    num_total = 0 
+    for key in dic:
+        num_total += dic[key]
+        if rand < num_total:
+            return key
+    
 
 def rand_test(hist):
-    ''' run the random word function a x times '''
+    ''' run the freq function a x times '''
     word = freq(hist)
     x = 10000
     output = []
@@ -41,33 +37,14 @@ def rand_test(hist):
 
     return histogram(output)
 
-    
-
-# one  => 0.125
-# fish => 0.5
-# two  => 0.125
-# red  => 0.125
-# blue => 0.125
-
-# def sample(lis):
-#     total = 0
-#     cum_prob = 0.0
-#     for val in lis:
-#         print(val[1])
-#         #total += val[1]
-    
-#     rand_num = random.uniform(0, 1)
-#     for val in lis:
-#         cum_prob += val[1]/total
-#         if cum_prob >= rand_num:
-#             return val[0]
-
-    
 if __name__ == '__main__':
+    # FUNCTIONS 
+    rand_word = single_rand_word(lines)
     hist = histogram(lines)
-    rand = freq(hist)
-    test = rand_test(hist)
+    rand = freq(lines)
+    test = rand_test(lines)
     
-    print(test)
+    # FUNCTION CALLS 
+    print('Randomness Test: {}'.format(test))
 
     

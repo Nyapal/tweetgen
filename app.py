343cb4cbb5078b7efb23146fedc83ca56ea09254
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-02-12 18:05:13 -0800

    getting ready to deploy to heroku

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask 
+from dictionary_words import sentence
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
-
+    sentence = sentence()
+    return sentence
 
 @app.route('/hello')
 def hello_world():
     return 'Hello World'
+
+
+if __name__ == '__main__':
+    index()
--- dictionary_words.py
+++ dictionary_words.py
@@ -5,6 +5,10 @@ from random import sample
 with open('//usr/share/dict/words', 'r') as f:
     words_list = [line.strip() for line in f]
 
-output = ' '.join(random.sample(words_list, 9)) + '.'
 
-print(output.capitalize())
+def sentence(): 
+    output = ' '.join(random.sample(words_list, 9)) + '.'
+    return output.capitalize()
+
+if __name__ == '__main__':
+    sentence()

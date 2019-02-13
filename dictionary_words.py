7f084c98bc0296a43a61832128dfffe438f5baae
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-02-12 18:36:49 -0800

    fixing

--- dictionary_words.py
+++ dictionary_words.py
@@ -2,7 +2,7 @@ import random
 from random import sample
 
 # with automatically closes the file once everything inside of it has been done
-with open('//usr/share/dict/words', 'r') as f:
+with open('sample.words', 'r') as f:
     words_list = [line.strip() for line in f]
 
 

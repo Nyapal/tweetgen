eb134e3105488470fe805cd28eaaa111a3c4f5de
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-02-15 15:32:07 -0800

    all test working but one

--- listogram.py
+++ listogram.py
@@ -19,15 +19,34 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        found = False 
+        for inner_tuple in self:
+            if word == inner_tuple[0]:
+                amount = inner_tuple[1] + count 
+                self.remove(inner_tuple)
+                self.append((word, amount))
+                found = True
+                break
+        if not found:
+            self.append((word, count))
+            self.types += 1
+        self.tokens += count 
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        for inner_tuple in self:
+            if word == inner_tuple[0]:
+                return inner_tuple[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for inner_tuple in self:
+            if word == inner_tuple[0]:
+                return True
+        return False
 
     def _index(self, target):
         """Return the index of entry containing given target word if found in
